77870a55b8c7800712d5a54153a45e2a9347a853
C0ldSt33l <sychev2003di@gmail.com>
2022-03-05 19:49:11 +0300

    Добавьте файлы проекта.

---
+++ Lab9/Lab9.1.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+
+/*19.Вычислить значения выражений у для значений x, равных 3, 6, …, 30:
+				y = t^3 + 3t^2 + 1, t =2 − x.*/
+
+int main()
+{
+	system("chcp 1251");
+	system("cls");
+
+	int  y, t;
+
+	for (int x = 30; x >= 3; x -= 3)
+	{
+		t = 2 - x;
+		y = t * t * t + 3 * t * t + 1;
+		printf("y = %d", y);
+	}
+
+	return 0;
+}
